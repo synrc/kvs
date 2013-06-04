958152faee3e514f50aaddd8642b7b810117a74d
Andrii Zadorozhnii <andrii.zadorozhnii@gmail.com>
2013-06-04 13:18:21 +0000

    update product record

--- include/products.hrl
+++ include/products.hrl
@@ -15,24 +15,21 @@
 
 -record(product, {
         id                     :: integer(), % auto
+        ext_id                 :: term(),    % ext
+        name                   :: binary(),  % admin (based on ext)
+        ext_name               :: binary(),  % ext
         vendor_id              :: integer(), % auto
         categories             :: list(integer()), % admin
-        ext_gift_id            :: term(),    % ext
-        gift_name              :: binary(),  % admin (based on ext)
-        ext_gift_name          :: binary(),  % ext
         description_short      :: binary(),  % admin (based on ext)
         description_long       :: binary(),  % admin (based on ext)
         image_small_url        :: binary(),  % admin (based on ext)
         image_big_url          :: binary(),  % admin (based on ext)
         publish_start_date     :: calendar:date_time(), % admin
         publish_end_date       :: calendar:date_time(), % admin
-        real_price             :: integer(), % ext
+        price                  :: integer(), % ext
         retailer_price         :: integer(), % ext
         our_price              :: integer(), % auto
-        in_stock,            % FIXME: WTF? ext
         enabled_on_site        :: boolean(), % admin
-        kakush_point           :: integer(), % auto
-        kakush_currency        :: integer(), % auto
         creation_date          :: calendar:date_time(), % auto
         modify_date            :: calendar:date_time() }).
 
