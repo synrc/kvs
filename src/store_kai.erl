a707c14bfd8bbcbbd4137e9f1e0ccb49ed2a1978
Maxim Sokhatsky <maxim@synrc.com>
2013-09-12 19:15:16 +0300

    mnesia

--- src/store_kai.erl
+++ src/store_kai.erl
@@ -16,7 +16,7 @@
 -include_lib("stdlib/include/qlc.hrl").
 -compile(export_all).
 
-start() -> kai_store:start(), ok.
+start() -> kai:start(), ok.
 stop() -> kai_store:stop(), ok.
 version() -> {version,"KVS KAI PURE XEN"}.
 join() -> initialize(), ok.
