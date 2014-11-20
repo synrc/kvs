59055a7e7ec763d591daa3ac801fe57f6594321e
Maxim Sokhatsky <maxim@synrc.com>
2014-11-20 23:27:33 +0200

    feed is default container for all chains

--- include/metainfo.hrl
+++ include/metainfo.hrl
@@ -2,6 +2,6 @@
 -define(METAINFO_HRL, true).
 
 -record(schema,{name,tables=[]}).
--record(table,{name,container,fields=[],keys=[],copy_type=disc_copies}).
+-record(table,{name,container=feed,fields=[],keys=[],copy_type=disc_copies}).
 
 -endif.
