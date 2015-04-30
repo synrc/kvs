7ec5f58967fd288c4db42d5ee9da2206b43ccfa1
Namdak Tonpa <maxim@synrc.com>
2015-04-30 06:36:11 +0300

    add group to metainfo boot

--- src/kvs_user.erl
+++ src/kvs_user.erl
@@ -1,6 +1,7 @@
 -module(kvs_user).
 -copyright('Synrc Research Center s.r.o.').
 -include("user.hrl").
+-include("group.hrl").
 -include("config.hrl").
 -include("metainfo.hrl").
 -compile(export_all).
@@ -8,6 +9,7 @@
 metainfo() ->
     #schema{name=kvs,tables=[
         #table{name=user2,container=feed,fields=record_info(fields,user2)},
+        #table{name=group,container=feed,fields=record_info(fields,group)},
         #table{name=user,container=feed,fields=record_info(fields,user),
                  keys=[facebook_id,googleplus_id,twitter_id,github_id]}
     ]}.
