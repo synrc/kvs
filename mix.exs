3cb5816e44a9926639d5ccddfa41ef4f8c9f8d49
5HT <maxim@synrc.com>
2019-12-18 13:40:03 +0200

    fix test

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule KVS.Mixfile do
   def project do
     [
       app: :kvs,
-      version: "6.12.7",
+      version: "6.12.8",
       description: "KVS Abstract Chain Database",
       package: package(),
       deps: deps()
