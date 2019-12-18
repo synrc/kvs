8ed7e3eeca94486fb0d29f6a7c825105c4ae3fae
5HT <maxim@synrc.com>
2019-12-18 14:43:47 +0200

    defined, fields, field promoted by @pal-alex

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule KVS.Mixfile do
   def project do
     [
       app: :kvs,
-      version: "6.12.8",
+      version: "6.12.9",
       description: "KVS Abstract Chain Database",
       package: package(),
       deps: deps()
