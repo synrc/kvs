e33e3fbcc5152bd29ba2dd7a9497ea2c39ced009
Oleksandr Naumov <oleksandr.naumov91@gmail.com>
2020-01-28 16:34:31 +0200

    7.1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule KVS.Mixfile do
   def project do
     [
       app: :kvs,
-      version: "7.1.2",
+      version: "7.1.3",
       description: "KVS Abstract Chain Database",
       package: package(),
       deps: deps()
