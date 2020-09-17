b7a1877e4c56319fc62f0661f590f15ffcb7358f
pal-alex <palchikovskiy.alexander@gmail.com>
2020-09-17 15:25:19 +0200

    a reader for rocks

--- include/cursors.hrl
+++ include/cursors.hrl
@@ -7,9 +7,11 @@
                   args  = [] :: term(),
                   first = [] :: [] | tuple() } ).
 -record(reader, { id    = [] :: [] | integer(),
-                  pos   =  0 :: integer() | atom(),
-                  cache = [] :: [] | integer() | {term(),term()},
-                  args  = 0 :: term(),
-                  feed  = [] :: term(),
-                  dir   =  0 :: 0 | 1 } ).
+                pos   =  0 :: integer() | atom(),
+                cache = [] :: [] | integer() | {term(),term()},
+                args  = 0 :: term(),
+                feed  = [] :: term(),
+                seek = [] :: term(),
+                count = 0 :: integer(),
+                dir   =  0 :: 0 | 1 } ).
 -endif.
