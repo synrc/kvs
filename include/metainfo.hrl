01f1f3e505f2b1ac55a2a93b3193d428ff3f05ea
tonpa <maxim@synrc.com>
2021-03-22 16:03:00 +0200

    8.3.0

--- include/metainfo.hrl
+++ include/metainfo.hrl
@@ -2,5 +2,5 @@
 -define(METAINFO_HRL, true).
 -record(schema, {name,tables=[]}).
 -record(table,  {name,container=false,type=set,fields=[],keys=[],
-                 copy_type=application:get_env(kvx,mnesia_media,disc_copies), instance={} }).
+                 copy_type=application:get_env(kvx,mnesia_media,disc_copies), instance={}, mappings =[] }).
 -endif.
