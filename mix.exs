90c5879554d577399bb799063aec347f612dd692
SunRiseGG <SunRiseGC4@gmail.com>
2022-04-26 18:55:44 +0300

    9.4.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule KVS.Mixfile do
   def project do
     [
       app: :kvs,
-      version: "9.4.1",
+      version: "9.4.3",
       description: "KVS Abstract Chain Database",
       package: package(),
       deps: deps()
