7c33ca96206a4600e9a83b41b455f434847b0838
iho <iho@users.noreply.github.com>
2026-03-01 00:20:54 +0200

    WIP

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,16 +1,20 @@
-ExUnit.start()
+ExUnit.start(exclude: [:rocks])
 
-defmodule :rocksdb do
-  def destroy(_, _), do: :ok
-  def iterator(_, _), do: {:ok, nil}
-  def iterator_close(_), do: :ok
-  def iterator_move(_, _), do: {:error, :not_found}
-  def open(_, _), do: {:ok, nil}
-  def put(_, _, _, _), do: :ok
-  def delete(_, _, _), do: :ok
-  def delete_range(_, _, _, _), do: :ok
-  def compact_range(_, _, _, _), do: :ok
-  def close(_), do: :ok
-  def get(_, _, _), do: :not_found
-  def get_property(_, _), do: {:ok, "0"}
+# Stub :rocksdb NIF so rocks.ex compiles and warnings are suppressed during Mnesia tests.
+# Real RocksDB tests (mix test.rocks) require the actual :rocksdb NIF to be compiled.
+unless Code.ensure_loaded?(:rocksdb) and function_exported?(:rocksdb, :open, 2) do
+  defmodule :rocksdb do
+    def destroy(_, _), do: :ok
+    def iterator(_, _), do: {:ok, nil}
+    def iterator_close(_), do: :ok
+    def iterator_move(_, _), do: {:error, :not_found}
+    def open(_, _), do: {:ok, nil}
+    def put(_, _, _, _), do: :ok
+    def delete(_, _, _), do: :ok
+    def delete_range(_, _, _, _), do: :ok
+    def compact_range(_, _, _, _), do: :ok
+    def close(_), do: :ok
+    def get(_, _, _), do: :not_found
+    def get_property(_, _), do: {:ok, "0"}
+  end
 end
